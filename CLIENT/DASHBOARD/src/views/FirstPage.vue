5d94e236ca31cbbcc4c869f70b809160e97f5e97
LBilheran <loisbilheran@outlook.fr>
2025-02-28 13:13:07 +0100

    Opérational Graphs with JSON

--- CLIENT/DASHBOARD/src/views/FirstPage.vue
+++ CLIENT/DASHBOARD/src/views/FirstPage.vue
@@ -2,47 +2,47 @@
 import Footer from '../components/Footer.vue'
 import Header from '../components/Header.vue'
 import MAP from '../components/leafletMap.vue'
-import Graphique from '../components/graphique.vue'
+import Graphs from '../components/Graphs.vue'
 
-// Exemple de données pour le graphique
-const timestamps = [
-  "2025-02-24T12:24:40.000Z",
-  "2025-02-24T12:25:00.000Z",
-  "2025-02-24T12:25:20.000Z",
-  "2025-02-24T12:25:40.000Z",
-  "2025-02-24T12:26:00.000Z",
-  "2025-02-24T12:26:20.000Z",
-];
-
-const values = [
-  9.99,
-  10.01,
-  10.05,
-  10.02,
-  10.00,
-  10.03,
-];
-
-// Nom du capteur et unité
-const sensorName = "Temperature";
-const unit = "°C";
+// Exemple de JSON avec des données pour plusieurs capteurs et des timestamps
+const jsonData = {
+  id: 30,
+  unit: {
+    temperature: "°C",
+    pressure: "hPa",
+    humidity: "%",
+  },
+  data: {
+    "2025-02-24T12:24:40.000Z": {
+      temperature: 9.99,
+      pressure: 994.96,
+      humidity: 50,
+    },
+    "2025-02-24T12:25:00.000Z": {
+      temperature: 10.01,
+      pressure: 995.01,
+      humidity: 51,
+    },
+    "2025-02-24T12:25:20.000Z": {
+      temperature: 10.05,
+      pressure: 995.05,
+      humidity: 52,
+    }
+  }
+}
 </script>
 
 <template>
   <header>
     <Header />
   </header>
-  <map>
+  <!-- <map>
     <MAP />
-  </map>
-  <div class="chart-page">
-    <Graphique 
-      :title="sensorName" 
-      :unit="unit" 
-      :timestamps="timestamps" 
-      :values="values" 
-    />
-  </div>
+  </map> -->
+  <graph>
+    <!-- Utilisation du nouveau composant Graphs avec le JSON fourni -->
+    <Graphs :graphsJson="jsonData"/>
+  </graph>
   <footer>
     <Footer />
   </footer>
